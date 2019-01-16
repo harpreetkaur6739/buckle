9bd962372cbe3c4ccff74b08447d1e0eceed70b9
Harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2019-01-16 17:16:52 -0500

    First Commit

---
+++ src/main/java/com/pro/buckle/controller/BuckleApplication.java
@@ -0,0 +1,14 @@
+package com.pro.buckle.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckleApplication.class, args);
+	}
+
+}
+
